419f6726e901d5c78c645c6ec0ee7af61431395e
Faouz <faouz5307@gmail.com>
2022-04-08 16:05:11 -0400

    Classificator test

--- classificator.py
+++ classificator.py
@@ -9,52 +9,66 @@ def unison_shuffled_copies(a, b):
     p = np.random.permutation(len(a))
     return a[p], b[p]
 
-array = np.random.rand(600, 32, 32)
 
-loaded_arr = np.loadtxt("data.csv")
-loaded_arr = loaded_arr[:600]
+img_size = 32
+# -------- Data loading --------------- #
+print('--- Loading data ...')
+# Load icon data from file
+loaded_ic_data = np.loadtxt("./ic_data/1000icons_x_5tags/ic_data_clean.csv")
+# Reshape to the correct img shape
+X = loaded_ic_data.reshape(
+        loaded_ic_data.shape[0], loaded_ic_data.shape[1] // img_size, img_size)
+# Load tags data from file
+Y = np.loadtxt("./ic_data/1000icons_x_5tags/tags_data_clean.csv")
+print(f'--- Data loaded : {len(X)} samples ---')
+# ------------------------------------- #
 
-X = loaded_arr.reshape(
-    loaded_arr.shape[0], loaded_arr.shape[1] // array.shape[2], array.shape[2])
-Y = np.loadtxt("tags_data.csv")
-Y = Y[:600]
+# ------ Create train and test datasets -----#
+train_test_ratio = 0.8
 
+print("--- Creating train and test datasets ---")
+train_size = int(train_test_ratio*len(X))
 X_shuffle, Y_shuffle = unison_shuffled_copies(X, Y)
-x_train = X_shuffle[:500]
-y_train = Y_shuffle[:500]
+x_train = X_shuffle[:train_size]
+y_train = Y_shuffle[:train_size]
 
-x_test = X_shuffle[500:]
-y_test = Y_shuffle[500:]
+x_test = X_shuffle[train_size:]
+y_test = Y_shuffle[train_size:]
+print("--- Train and test datasets created ---")
+print(f"--- Train dataset : {len(x_train)} samples | Test dataset : {len(x_test)} samples ---")
+# -----------------------------------------#
 
-
-num_classes = 23
-input_shape = (32, 32, 1)
+# --------- Create model --------------- #
+num_classes = np.shape(y_train)[1]
+input_shape = (img_size, img_size, 1)
 
 model = keras.Sequential(
     [
-        keras.Input(shape=input_shape),  # -> 28x28x1
-        layers.Conv2D(16, kernel_size=(3, 3), activation="relu", padding="same"),  # -> 28x28x16
-        layers.MaxPooling2D(pool_size=(2, 2)),  # -> 6x6x32
+        keras.Input(shape=input_shape),
+        layers.Conv2D(16, kernel_size=(3, 3), activation="relu", padding="same"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Conv2D(64, kernel_size=(3, 3),
                       kernel_initializer="lecun_normal",
-                      activation="selu", padding="same"),  # -> 6x6x64
-        layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),  # -> 3x3x64
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu", strides=(3, 3)),  # -> 1x1x64
-        layers.Flatten(),  # -> 64
-        layers.Dropout(0.5),  # -> 64
-        layers.Dense(num_classes, activation="sigmoid"),  # -> 10
+                      activation="selu", padding="same"),
+        layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu", strides=(3, 3)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="sigmoid"),
     ]
 )
-
 model.summary()
+# -------------------------------------------------- #
 
-batch_size = 20
+# ---------------- Train --------------------------#
+batch_size = 100
 epochs = 100
 
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+# ------------------------------------------------- #
 
+# ------------- Test ------------------------------ #
 # make a prediction on the test set
 yhat = model.predict(x_test)
 # round probabilities to class labels
@@ -67,3 +81,4 @@ print('>%.3f' % acc)
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
+# ----------------------------------------------- #
